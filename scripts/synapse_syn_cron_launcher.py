01befea600886bf341a43e806a47c19e358dcb18
J466Y <agerkalboetxeaga@gmail.com>
2026-04-20 11:01:23 +0000

    Updated Cron launcher with API Key

--- scripts/synapse_syn_cron_launcher.py
+++ scripts/synapse_syn_cron_launcher.py
@@ -5,6 +5,9 @@ MODULES = [
 	{"name": "fortiedr", "timerange": 30}
     # {"name": "splunk", "timerange": 15}, # Facil de añadir en el futuro
 ]
+
+TOKEN = "Your Token"
+
 SYNAPSE_URL = "http://127.0.0.1:5000/integration/"
 def run_sync():
     for module in MODULES:
@@ -12,7 +15,8 @@ def run_sync():
         try:
             url = f"{SYNAPSE_URL}{module['name']}"
             data = {"timerange": module['timerange']}
-            response = requests.post(url, json=data)
+            headers = {"Authorization": f"Bearer {TOKEN}"}
+            response = requests.post(url, json=data, headers=headers)
             print(f"Resultado: {response.status_code}")
         except Exception as e:
             print(f"Error en {module['name']}: {e}")
