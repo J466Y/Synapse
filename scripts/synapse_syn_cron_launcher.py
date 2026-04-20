2007e5c284f569b9f9b013e7ebb0f7288e8bba62
J466Y <agerkalboetxeaga@gmail.com>
2026-04-20 14:21:11 +0000

    Added conf logic

--- scripts/synapse_syn_cron_launcher.py
+++ scripts/synapse_syn_cron_launcher.py
@@ -1,4 +1,14 @@
+import os
+import sys
 import requests
+
+# Set project root in sys.path to allow importing core modules
+synapse_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if synapse_dir not in sys.path:
+    sys.path.append(synapse_dir)
+
+from core.functions import getConf
+
 # Lista de módulos que quieres sincronizar
 MODULES = [
 	{"name": "qradar", "timerange": 30},
@@ -6,8 +16,8 @@ MODULES = [
     # {"name": "splunk", "timerange": 15}, # Facil de añadir en el futuro
 ]
 
-TOKEN = "Your Token"
-
+cfg = getConf()
+TOKEN = cfg.get('api', 'api_key')
 SYNAPSE_URL = "http://127.0.0.1:5000/integration/"
 def run_sync():
     for module in MODULES:
